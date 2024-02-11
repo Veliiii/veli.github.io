44266cb21dc15d48b88fc6d90ec72974d0fce0e7
Veliiii <65997577+Veliiii@users.noreply.github.com>
2024-02-11 12:28:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.getElementById("hangoutButton").addEventListener("click", function() {
 
     // Show the alert message after a short delay
     setTimeout(function() {
-        alert("Yay, I'm so happy");
+        alert("I'm glad u finally admitted that");
     }, 500); // Show the alert message after 500 milliseconds
 });
 
